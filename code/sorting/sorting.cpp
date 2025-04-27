54cfc4a6fc2165180f0e90ed3f8cb4257934df31
ToonLucas22 <lucas.ignacio.morrison.osorio@gmail.com>
2025-04-27 03:26:06 -0400

    ups

--- code/sorting/sorting.cpp
+++ code/sorting/sorting.cpp
@@ -10,40 +10,6 @@
 #include "stdio.h"
 #include "string.h"
 
-/*
-    Las siguientes funciones parseLine y getValue han sido tomadas
-    de esta página de stackoverflow:
-    https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
-
-    Su propósito es parsear el valor arrojado por el kernel correspondiente
-    a la memoria usada por el proceso actual y luego retornar tal valor.
-*/
-
-int parseLine(char* line){
-    // This assumes that a digit will be found and the line ends in " Kb".
-    int i = strlen(line);
-    const char* p = line;
-    while (*p <'0' || *p > '9') p++;
-    line[i-3] = '\0';
-    i = atoi(p);
-    return i;
-}
-
-int getValue(){ //Note: this value is in KB!
-    FILE* file = fopen("/proc/self/status", "r");
-    int result = -1;
-    char line[128];
-
-    while (fgets(line, 128, file) != NULL){
-        if (strncmp(line, "VmRSS:", 6) == 0){
-            result = parseLine(line);
-            break;
-        }
-    }
-    fclose(file);
-    return result;
-}
-
 int main() {
     int N[] = {10, 1000, 100000, 10000000};
     std::string T[] = {"ascendente", "descendente", "aleatorio"};
