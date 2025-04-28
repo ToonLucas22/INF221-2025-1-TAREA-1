ac286a788d1bda2a1e24a4356176e82242960e7e
ToonLucas22 <lucas.ignacio.morrison.osorio@gmail.com>
2025-04-28 13:23:18 -0400

    Update plot_generator.py

--- code/sorting/scripts/plot_generator.py
+++ code/sorting/scripts/plot_generator.py
@@ -14,67 +14,67 @@ def autolabel(rects):
                     textcoords="offset points",
                     ha='center', va='bottom')
 
-sorts = ['SelectionSort', 'MergeSort', 'QuickSort', 'std::sort']
-sizes = [10, 1000, 100000, 10000000]
-indices = [0, 73, 145, 217, 271]
-doms = ['D1', 'D7']
+mults = ['Strassen', 'Naive']
+sizes = [16, 64, 256, 1024]
+indices = [1, 37, 73, 109, 145]
+doms = ['D1', 'D10']
 
 measures = open("../data/measurements/a.txt")
 
 for i in range(4):
-  cntAsc1 = [0]*4
-  cntDes1 = [0]*4
-  cntRnd1 = [0]*4
-  cntAsc7 = [0]*4
-  cntDes7 = [0]*4
-  cntRnd7 = [0]*4
-  avgTimeAsc1 = [0]*4
-  avgTimeDes1 = [0]*4
-  avgTimeRnd1 = [0]*4
-  avgTimeAsc7 = [0]*4
-  avgTimeDec7 = [0]*4
-  avgTimeRnd7 = [0]*4
+  cntDsp1 = [0]*2
+  cntDgn1 = [0]*2
+  cntDen1 = [0]*2
+  cntDsp10 = [0]*2
+  cntDgn10 = [0]*2
+  cntDen10 = [0]*2
+  avgTimeDsp1 = [0]*2
+  avgTimeDgn1 = [0]*2
+  avgTimeDen1 = [0]*2
+  avgTimeDsp10 = [0]*2
+  avgTimeDgn10 = [0]*2
+  avgTimeDen10 = [0]*2
   n = sizes[i]
   for j in range(indices[i], indices[i+1]):
     line = measures.readline().strip().split()
     modes = filename[0].strip(".txt").split("_")
-    sortMode = sorts.find(line[1])
+    multMode = mults.find(line[1])
     type = modes[1]
     d = modes[2]
     eTime = float(line[2])
     if d == "D1":
-      if type == 'ascendente':
-        cntAsc1[sortMode] += 1
-        avgTimeAsc1[sortMode] += eTime
-      elif type == 'descendente':
-        cntDes1[sortMode] += 1
-        avgTimeDes1[sortMode] += eTime
+      if type == 'dispersa':
+        cntDsp1[multMode] += 1
+        avgTimeDgn1[sortMode] += eTime
+      elif type == 'diagonal':
+        cntDgn1[multMode] += 1
+        avgTimeDgn1[sortMode] += eTime
       else:
-        cntRnd1[sortMode] += 1
-        avgTimeRnd1[sortMode] += eTime
+        cntDen1[multMode] += 1
+        avgTimeDen1[sortMode] += eTime
     else:
-      if type == 'ascendente':
-        cntAsc7[sortMode] += 1
-        avgTimeAsc7[sortMode] += eTime
-      elif type == 'descendente':
-        cntDes7[sortMode] += 1
-        avgTimeDes7[sortMode] += eTime
+      if type == 'dispersa':
+        cntDsp10[multMode] += 1
+        avgTimeDsp10[sortMode] += eTime
+      elif type == 'diagonal':
+        cntDgn10[multMode] += 1
+        avgTimeDgn10[sortMode] += eTime
       else:
-        cntRnd7[sortMode] += 1
-        avgTimeRnd7[sortMode] += eTime
-  for j in range(4):
-    if (cntAsc1[j] != 0):
-      avgTimeAsc1[j] /= cntAsc1[j]
-    if (cntDes1[j] != 0):
-      avgTimeDes1[j] /= cntDes1[j]
+        cntDen10[multMode] += 1
+        avgTimeDen10[sortMode] += eTime
+  for j in range(2):
+    if (cntDsp1[j] != 0):
+      avgTimeDsp1[j] /= cntDsp1[j]
+    if (cntDgn1[j] != 0):
+      avgTimeDgn1[j] /= cntDgn1[j]
     if (cntRnd1[j] != 0):
-      avgTimeRnd1[j] /= cntRnd1[j]
-    if (cntAsc7[j] != 0):
-      avgTimeAsc7[j] /= cntAsc7[j]
-    if (cntDes7[j] != 0)
-      avgTimeDes7[j] /= cntDes7[j]
-    if (cntRnd7[j] != 0)
-      avgTimeRnd7[j] /= cntRnd7[j]
+      avgTimeDen1[j] /= cntDen1[j]
+    if (cntDsp10[j] != 0):
+      avgTimeDsp10[j] /= cntDsp10[j]
+    if (cntDgn10[j] != 0)
+      avgTimeDgn10[j] /= cntDgn10[j]
+    if (cntDen10[j] != 0)
+      avgTimeDen10[j] /= cntDen10[j]
     
   # El siguiente código es mayormente tomado del sitio
   # https://facialix.com/tutorial-creacion-de-graficas-en-python-usando-matplotlib/
@@ -85,19 +85,19 @@ for i in range(4):
   for j in range(2):
     fig, ax = plt.subplots()
     if j==0:
-      toUseA = avgTimeAsc1
-      toUseD = avgTimeDes1
-      toUseR = avgTimeRnd1
+      toUseA = avgTimeDsp1
+      toUseD = avgTimeDgn1
+      toUseR = avgTimeDen1
     else:
-      toUseA = avgTimeAsc7
-      toUseD = avgTimeDes7
-      toUseR = avgTimeRnd7
-    rectsAsc = ax.bar(x - width, toUseA, width, label='Ascendente')
-    rectsDes = ax.bar(x,         toUseD, width, label='Descendente')
-    rectsRnd = ax.bar(x + width, toUseR, width, label='Aleatorio')
+      toUseA = avgTimeDsp10
+      toUseD = avgTimeDgn10
+      toUseR = avgTimeDen10
+    rectsAsc = ax.bar(x - width, toUseA, width, label='Dispersa')
+    rectsDes = ax.bar(x,         toUseD, width, label='Diagonal')
+    rectsRnd = ax.bar(x + width, toUseR, width, label='Densa')
 
     ax.set_ylabel('Tiempo de ejecución (s)')
-    ax.set_title('Tiempo de ejecución de algoritmos de ordenamiento en '+doms[j])
+    ax.set_title('Tiempo de ejecución de multiplicación de matrices en '+doms[j])
     ax.set_xticks(x)
     ax.set_xticklabels(sorts)
     ax.legend()
